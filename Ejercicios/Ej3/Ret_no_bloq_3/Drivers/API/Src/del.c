2a8e2f2b0371b68490395a19629ec6435fb11b28
matutecno <mmorales2507@gmail.com>
2026-03-24 10:34:39 -0300

    Actualiza TP3

---
+++ Ejercicios/Ej3/Ret_no_bloq_3/Drivers/API/Src/del.c
@@ -0,0 +1,27 @@
+#include "del.h"
+
+void delayInit( delay_t * delay, tick_t duration ){
+	delay->running = true;
+	delay->startTime = HAL_GetTick();
+	delay->duration = duration;
+};
+
+bool_t delayRead( delay_t * delay ){
+
+	uint32_t aux = HAL_GetTick() - delay->startTime;
+	if(aux >= delay->duration)
+		delay->running = false;
+
+	return delay->running;
+};
+
+
+void delayWrite( delay_t * delay, tick_t duration ){
+	if(!(delay->running)){
+		delayInit(delay, duration);
+	}
+};
+
+bool_t delayIsRunning(delay_t * delay){
+	return delay->running;
+};

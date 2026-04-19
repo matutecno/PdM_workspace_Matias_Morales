abf3eb9c6c6160fbc6eb2195add6f57e882ddf5a
matutecno <mmorales2507@gmail.com>
2026-04-19 18:23:33 -0300

    Update gral

--- TPF/Master_0/Master_0/Core/Inc/main.h
+++ TPF/Master_0/Master_0/Core/Inc/main.h
@@ -28,10 +28,10 @@ extern "C" {
 
 /* Includes ------------------------------------------------------------------*/
 #include "stm32f1xx_hal.h"
-#include "ports_master.h"
+
 /* Private includes ----------------------------------------------------------*/
 /* USER CODE BEGIN Includes */
-
+#include "ports_master.h"
 /* USER CODE END Includes */
 
 /* Exported types ------------------------------------------------------------*/
